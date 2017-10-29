a989f9820fe4fcf4205df4f24a281b91297dfb4b
Ben Wibrew <benjamin.wibrew@gmail.com>
2017-10-29 19:08:56 +0000

    Add base plugin files
    Add meta files
    
    
    Add unit testing

---
+++ wp-site-monitor.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:  WP Site Monitor
+ * Plugin URI:   https://github.com/BWibrew/WP-Site-Monitor/
+ * Description:  Provides extra WP REST API endpoints to help manage sites remotely.
+ * Version:      0.1.0
+ * Author:       Benjamin Wibrew
+ * Author URI:   https://github.com/BWibrew/
+ * License:      MIT License
+ * License URI:  http://opensource.org/licenses/MIT
+ * Text Domain:  wp-site-monitor
+ * Domain Path:  /languages
+ */
