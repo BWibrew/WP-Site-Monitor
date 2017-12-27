8429c082a9cb02263e6a206f2ef7558e1cdbf7d2
Ben Wibrew <benjamin.wibrew@gmail.com>
2017-12-27 23:10:01 +0000

    Add first test

---
+++ tests/test-settings-page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Class SettingsPageTest
+ *
+ * @package WPSiteMonitor
+ * @since 1.0.0
+ */
+
+/**
+ * Settings Page test case.
+ */
+class SettingsPageTest extends WP_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+
+		wp_set_current_user( self::factory()->user->create( array( 'role' => 'administrator' ) ) );
+	}
+
+	/**
+	 * Assert that the settings page is added to the settings menu successfully.
+	 */
+	function test_settings_page_is_added_to_settings_menu() {
+		$settings_menu = new \WPSiteMonitor\Settings_Menu();
+		$settings_menu->display_settings_page();
+
+		$this->assertEquals( admin_url() . 'options-general.php?page=wp_site_monitor', menu_page_url( 'wp_site_monitor', false ) );
+	}
+}
