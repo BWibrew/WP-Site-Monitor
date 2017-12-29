2f901da3f7c4e7f54f4c4793ec0b9de8bb3dcb37
Ben Wibrew <benjamin.wibrew@gmail.com>
2017-12-29 12:43:30 +0000

    Improve tests

--- tests/test-settings-menu.php
+++ tests/test-settings-menu.php
@@ -6,24 +6,33 @@
  * @since 1.0.0
  */
 
+use WPSiteMonitor\Settings_Menu;
+
 /**
  * Settings Page test case.
  */
 class SettingsMenuTest extends WP_UnitTestCase {
 
-	function setUp() {
+	protected $settings_menu;
+	protected $menu_slug;
+
+	public function setUp() {
 		parent::setUp();
 
+		$this->settings_menu = new Settings_Menu();
+		$this->menu_slug = 'wp_site_monitor';
 		wp_set_current_user( self::factory()->user->create( array( 'role' => 'administrator' ) ) );
 	}
 
 	/**
 	 * Assert that the settings page is added to the settings menu successfully.
 	 */
-	function test_settings_page_is_added_to_settings_menu() {
-		$settings_menu = new \WPSiteMonitor\Settings_Menu();
-		$settings_menu->display_settings_page();
+	public function test_settings_page_is_added_to_settings_menu() {
+		global $submenu;
+		$this->settings_menu->display_settings_page();
 
-		$this->assertEquals( admin_url() . 'options-general.php?page=wp_site_monitor', menu_page_url( 'wp_site_monitor', false ) );
+		$this->assertEquals( admin_url() . 'options-general.php?page=' . $this->menu_slug, menu_page_url( $this->menu_slug, false ) );
+		$this->assertEquals( 'WP Site Monitor Settings', $submenu['options-general.php'][0][3] );
+		$this->assertEquals( 'WP Site Monitor', $submenu['options-general.php'][0][0] );
 	}
 }
